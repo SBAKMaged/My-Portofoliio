da2f7d3e6cd0d5a22ca4b2903034e04c6d38f810
Mohammed Maged <mohamed.ibn.maged@gmail.com>
2023-12-24 02:12:04 +0200

    Adding-Animations

--- script.js
+++ script.js
@@ -4,3 +4,35 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+
+// Function to check if an element is in the viewport
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+  const threshold = 0.2; // Adjust this value to change when the animation triggers
+
+  return (
+    rect.bottom >= 0 &&
+    rect.top <= windowHeight * threshold
+  );
+}
+
+// Function to handle scroll animations
+function handleScrollAnimations() {
+  const animatedSections = document.querySelectorAll('.scroll-animation');
+
+  animatedSections.forEach(section => {
+    if (isInViewport(section)) {
+      section.classList.add('animate');
+    }
+  });
+}
+
+// Event listener for scroll event
+window.addEventListener('scroll', () => {
+  handleScrollAnimations();
+});
+
+// Initial check for animations on page load
+handleScrollAnimations();
